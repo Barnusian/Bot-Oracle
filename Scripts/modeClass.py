cf23daab4b2bc51475e75246e858616d43af1463
Barnie Mumby-Price <barneymp@gmail.com>
2025-06-17 23:08:38 +0100

    completed resource dictionary refactoring

--- Scripts/modeClass.py
+++ Scripts/modeClass.py
@@ -15,57 +15,57 @@ class PrintMode:
         if (os.path.exists(self.image_path)) :
             return Image.open(self.image_path)
         else :
-            return Image.open("Images\lemonpig.png")
+            return Image.open("Images/lemonpig.png")
 
 # Dictionary of modes
 modes = {
     # Helvetica on frame    
     1 : {
-        "image" : "Images/decorative frame.png",
-        "text" : "Text/random-text.txt",
-        "font" : "Fonts/Helvetica-Bold.ttf",
-        "font size" : 72,
-        "vertical offset" : 7,
+        "image_path" : "Images/decorative frame.png",
+        "text_path" : "Text/random-text.txt",
+        "font_path" : "Fonts/Helvetica-Bold.ttf",
+        "font_size" : 72,
+        "offset" : 7,
         "dither" : False
     },
 
     # Star wars guy, vertical text
     2 : {
-        "image" : "Images/dancers.png",
-        "text" : "Text/random-text.txt",
-        "font" : "Fonts/Helvetica-Bold.ttf",
-        "font size" : 60,
-        "vertical offset" : 340,
+        "image_path" : "Images/dancers.png",
+        "text_path" : "Text/random-text.txt",
+        "font_path" : "Fonts/Helvetica-Bold.ttf",
+        "font_size" : 60,
+        "offset" : 340,
         "dither" : True
     },
 
     # Manga image, text box
     3 : {
-        "image" : "Images/bride of dracula.png",
-        "text" : "Text/dressforaday.txt",
-        "font" : "Fonts/FreeMonospaced-7ZXP.ttf",
-        "font size" : 50,
-        "vertical offset" : 340,
+        "image_path" : "Images/bride of dracula.png",
+        "text_path" : "Text/dressforaday.txt",
+        "font_path" : "Fonts/FreeMonospaced-7ZXP.ttf",
+        "font_size" : 50,
+        "offset" : 340,
         "dither" : False
     },
 
     # cat dog image
     4 : {
-        "image" : "Images/catndog.png",
-        "text" : "Text/bot-oracle.txt",
-        "font" : "Fonts/Helvetica-Bold.ttf",
-        "font size" : 30,
-        "vertical offset" : 200,
+        "image_path" : "Images/catndog.png",
+        "text_path" : "Text/bot-oracle.txt",
+        "font_path" : "Fonts/Helvetica-Bold.ttf",
+        "font_size" : 30,
+        "offset" : 200,
         "dither" : False
     },
 
     # Bartina
     5 : {
-        "image" : "Images/Bartina.png",
-        "text" : "Text/barts-words.txt",
-        "font" : "Fonts/Helvetica-Bold.ttf",
-        "font size" : 35,
-        "vertical offset" : -158,
+        "image_path" : "Images/Bartina.png",
+        "text_path" : "Text/barts-words.txt",
+        "font_path" : "Fonts/Helvetica-Bold.ttf",
+        "font_size" : 35,
+        "offset" : -158,
         "dither" : True
     }
 }
