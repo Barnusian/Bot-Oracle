65bdc08692dda42db3117a39efa39c04c6543973
Barnie Mumby-Price <barneymp@gmail.com>
2025-07-19 01:25:42 +0100

    fix: removed if __name__ block from clear_matrix

--- Scripts/LEDs/clear_matrix.py
+++ Scripts/LEDs/clear_matrix.py
@@ -1,10 +1,7 @@
-from device_setup import get_device
+from Scripts.LEDs.device_setup import get_device
 from luma.core.render import canvas
 
 def clear_display():
     device = get_device()
     with canvas(device):
         pass  # clears display by drawing nothing
-
-if __name__ == "__main__":
-    clear_display()
